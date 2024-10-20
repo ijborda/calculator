aeef09b1c0317e9cba7aa514ec147c3fc241a5ac
Imari Joy Borda <37444801+ijborda@users.noreply.github.com>
2024-10-20 13:06:20 +0800

    add underscores on tax rates

--- src/constants/tax-rate.ts
+++ src/constants/tax-rate.ts
@@ -1,37 +1,43 @@
 export const TAX_RATES = [
   {
     // 0%
-    inclusiveBounds: [0, 250000],
+    id: '1st',
+    inclusiveBounds: [0, 250_000],
     fixedTax: 0,
     excessTaxRate: 0,
   },
   {
     // 20% of the excess over P250,000
-    inclusiveBounds: [250000 + 1, 400000],
+    id: '2nd',
+    inclusiveBounds: [250_000 + 1, 400_000],
     fixedTax: 0,
     excessTaxRate: 0.2,
   },
   {
     // P30,000 + 25% of the excess over P400,000
-    inclusiveBounds: [400000 + 1, 800000],
+    id: '3rd',
+    inclusiveBounds: [400_000 + 1, 800_000],
     fixedTax: 30000,
     excessTaxRate: 0.25,
   },
   {
     // P130,000 + 30% of the excess over P800,000
-    inclusiveBounds: [800000 + 1, 20000000],
+    id: '4th',
+    inclusiveBounds: [800_000 + 1, 2_000_000],
     fixedTax: 130_000,
     excessTaxRate: 0.3,
   },
   {
     // P490,000 + 32% of the excess over P2,000,000
-    inclusiveBounds: [20000000 + 1, 8000000],
+    id: '5th',
+    inclusiveBounds: [2_000_0000 + 1, 8_000_000],
     fixedTax: 490_000,
     excessTaxRate: 0.32,
   },
   {
     // P2,410,000 + 35% of the excess over P8,000,000
-    inclusiveBounds: [8000000 + 1, Infinity],
+    id: '6th',
+    inclusiveBounds: [8_000_000 + 1, Infinity],
     fixedTax: 2_410_000,
     excessTaxRate: 0.35,
   },
